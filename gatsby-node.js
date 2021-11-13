e23999e039d34135abfe5180619fb75f2328630b
leesky <leehaneul0623@gmail.com>
2021-11-13 15:03:08 +0900

    Update node config

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,12 +1,12 @@
-// exports.createPages = async ({ actions }) => {
-//   const { createPage } = actions
-//   createPage({
-//     path: "/using-dsg",
-//     component: require.resolve("./src/templates/using-dsg.js"),
-//     context: {},
-//     defer: true,
-//   })
-// }
+exports.createPages = async ({ actions }) => {
+    const { createPage } = actions
+    createPage({
+        path: "/using-dsg",
+        component: require.resolve("./src/pages/index.tsx"),
+        context: {},
+        defer: true,
+    })
+}
 exports.onCreateWebpackConfig = ({ stage, actions }) => {
     if (stage.startsWith("build-javascript")) {
         actions.setWebpackConfig({
